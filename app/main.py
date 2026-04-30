7c5bc6c9e9dfc89acb93463375f29ad4b0b582b6
MENH Chanheng <chanhengmenh@gmail.com>
2026-04-30 13:38:04 +0700

    add version and description to FastAPI app

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
-app = FastAPI(title="Student Management API")
+app = FastAPI(title="Student Management API", version="2.0.0", description="AUPP Student CRUD System")
 
 Instrumentator().instrument(app).expose(app)
 
